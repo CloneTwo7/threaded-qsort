5d2c110cd27aafc9afcbda3c0d630256156364e3
CloneTwo7 <travisp0027@gmail.com>
2022-11-01 18:53:36 +0000

    created repo

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <sys/time.h>
+#include "assignment7.h"
+
+
+char *getNextWord(FILE *fd);
+
+int main(int argc, char **argv) {
+	FILE *fp = fopen(argv[1], "r");
+	assert(fp != NULL);
+	char *arr[10000];
+
+	struct timeval start, end;
+	char *newWord;
+	int index = 0;
+	while ( ((newWord = getNextWord(fp)) != NULL) && index < 10000) {
+		arr[index] = calloc(1,strlen(newWord)+1);
+		strcpy(arr[index], newWord);
+		index++;
+	}
+	setSortThreads(2);
+
+	gettimeofday(&start, NULL);
+	sortThreaded(arr, index);
+	gettimeofday(&end, NULL);
+
+	for(int i = 0; i < index; i++) {
+		printf("%s\n", arr[i]);
+	}
+
+
+	int elapsed = ((end.tv_sec - start.tv_sec) * 1000000) + (end.tv_usec - start.tv_usec);
+	printf("Time taken to sort: %dms\n", elapsed);
+}
